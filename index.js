f0e95bfd21cf97034a6c1480e6c12ed06e55fec6
Maria Rios <kanelapr@gmail.com>
2022-06-28 18:10:11 -0400

    added some variables

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// variables
+
 const space = document.querySelectorAll(".grid-container");
 const PlayerX = 'X';
 const PlayerO = 'O';
+
+const victoryCheck = Array(space.length);
+victoryCheck.fill(null);
+
+//elements of the game
+
+const strike = document.getElementById("strike");
+const scoreArea = document.getElementById("score-area");
+const gameOver = document.getElementById("endgame-text");
+const playAgain = document.getElementById("play-again");
