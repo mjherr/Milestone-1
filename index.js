ec249a028c4370ad3fe4c75bfc3010b3b86f2d2e
Maria Rios <kanelapr@gmail.com>
2022-06-28 19:02:36 -0400

    added turn variable

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const space = document.querySelectorAll(".grid-container");
 const PlayerX = 'X';
 const PlayerO = 'O';
+const turn = PlayerX;
 
 const victoryCheck = Array(space.length);
 victoryCheck.fill(null);
@@ -13,3 +14,19 @@ const strike = document.getElementById("strike");
 const scoreArea = document.getElementById("score-area");
 const gameOver = document.getElementById("endgame-text");
 const playAgain = document.getElementById("play-again");
+
+//clicks functions
+
+box.forEach((boxes) => boxes.addEventListener ("click", boxClick));
+
+function boxClick(event) {
+    if (scoreArea.classList.contains('visible')) {
+        return;
+    }
+    const boxes = event.target;
+    const boxNumber = boxes.dataset.index;
+    if (boxes.innerText != ""){
+        return;
+    }
+    if (turn)
+}
