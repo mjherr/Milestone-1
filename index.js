5a2975ab21d2cbc34ff8129d30cf74228a6d267e
Maria Rios <kanelapr@gmail.com>
2022-06-30 19:18:23 -0400

    winning rows added

--- index.js
+++ index.js
@@ -38,4 +38,13 @@ function boxClick(event) {
         victoryCheck[boxNumber - 1] = PlayerO;
         turn = PlayerX;
     }
+
 }
+
+//winning rows
+
+const winningCombos = [
+    {combo: [1, 2, 3], strike: "strike-row-1"},
+    {combo: [4, 5, 6], strike: "strike-row-2"},
+    {combo: [7, 8, 9], strike: "strike-row-3"},
+]
