a3185e07ce53874a053350c9c12ba5cf6a5612c4
Maria Rios <kanelapr@gmail.com>
2022-07-03 09:21:49 -0400

    made some changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const space = document.querySelectorAll(".boxes");
 const PlayerX = "X";
 const PlayerO = "O";
 let turn = PlayerX;
+// let turn2 = PlayerO;
 
 const victoryCheck = Array(space.length);
 victoryCheck.fill(null);
@@ -38,19 +39,40 @@ function boxClick(event) {
         victoryCheck[boxNumber - 1] = PlayerO;
         turn = PlayerX;
     }
-    checkWinner();
+    checkForVictory();
 }
 
-function checkWinner() {
+function checkForVictory() {
     for (const winningCombo of winningCombos) {
-        console.log(winningCombo);
+        const {combo, strikes} = winningCombo;
+
+        const boxValue1 = victoryCheck[combo[0] - 1];
+        const boxValue2 = victoryCheck[combo[1] - 1];
+        const boxValue3 = victoryCheck[combo[2] - 1];
+
+        if(
+            boxValue1 != null && 
+            boxValue1 === boxValue2 && 
+            boxValue1 === boxValue3
+        ) {
+            strike.classList.add(strikes);
+        }
     }
 }
+
 //winning lines
 
 const winningCombos = [
-    //rows
-    {combo: [1, 2, 3], strike: "strike-row-1"},
-    {combo: [4, 5, 6], strike: "strike-row-2"},
-    {combo: [7, 8, 9], strike: "strike-row-3"},
+    //winning rows
+    {combo: [1, 2, 3], strikes: "strike-row-1"},
+    {combo: [4, 5, 6], strikes: "strike-row-2"},
+    {combo: [7, 8, 9], strikes: "strike-row-3"},
+    //winning columns
+    {combo: [1, 4, 7], strikes: "strike-column-1"},
+    {combo: [2, 5, 8], strikes: "strike-column-2"},
+    {combo: [3, 6, 9], strikes: "strike-column-3"},
+    //winning diagonals
+    {combo: [1, 5, 9], strikes: "strike-diagonal1"},
+    {combo: [3, 5, 7], strikes: "strike-diagonal2"},
+    
 ]
